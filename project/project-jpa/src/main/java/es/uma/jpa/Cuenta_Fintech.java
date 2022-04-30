c21bad1da2562fb988ca334aba9b3a62ddf2f2ae
psychys <swaglord@uma.es>
2022-04-30 21:02:12 +0200

    Quitado la clave rowId de cuenta fintech

--- project/project-jpa/src/main/java/es/uma/jpa/Cuenta_Fintech.java
+++ project/project-jpa/src/main/java/es/uma/jpa/Cuenta_Fintech.java
@@ -8,9 +8,9 @@ import javax.persistence.*;
 
 @Entity
 public class Cuenta_Fintech extends Cuenta{
-	@Id
+	/*@Id
 	@Column(name = "RowId")
-	private String rowId;
+	private String rowId;*/
 	@Temporal(TemporalType.DATE)
 	@Column(nullable = false)
 	private Date fecha_apertura;
@@ -26,12 +26,12 @@ public class Cuenta_Fintech extends Cuenta{
 	}
 
 	public Cuenta_Fintech(String IBAN, String SWIFT, String estado,
-						  Date fecha_apertura, Date fecha_cierre, String clasificacion, String rowId) {
+						  Date fecha_apertura, Date fecha_cierre, String clasificacion){//, String rowId) {
 		super(IBAN, SWIFT, estado);
 		this.fecha_apertura = fecha_apertura;
 		this.fecha_cierre = fecha_cierre;
 		this.clasificacion = clasificacion;
-		this.rowId = rowId;
+		//this.rowId = rowId;
 	}
 
 	public Cliente getCliente() {
@@ -63,15 +63,15 @@ public class Cuenta_Fintech extends Cuenta{
 		this.clasificacion = clasificacion;
 	}
 
-	public String getRowId() {
+	/*public String getRowId() {
 		return rowId;
 	}
 
 	public void setRowId(String rowId) {
 		this.rowId = rowId;
-	}
+	}*/
 
-	@Override
+	/*@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
 		if (o == null || getClass() != o.getClass()) return false;
@@ -90,6 +90,28 @@ public class Cuenta_Fintech extends Cuenta{
 		return "Cuenta_Fintech{" +
 				"rowId='" + rowId + '\'' +
 				", fecha_apertura=" + fecha_apertura +
+				", fecha_cierre=" + fecha_cierre +
+				", clasificacion='" + clasificacion + '\'' +
+				", cliente=" + cliente +
+				'}';
+	}*/
+
+	public boolean equals(Object object) {
+		if (this == object) return true;
+		if (object == null || getClass() != object.getClass()) return false;
+		if (!super.equals(object)) return false;
+		Cuenta_Fintech that = (Cuenta_Fintech) object;
+		return java.util.Objects.equals(fecha_apertura, that.fecha_apertura) && java.util.Objects.equals(fecha_cierre, that.fecha_cierre) && java.util.Objects.equals(clasificacion, that.clasificacion) && java.util.Objects.equals(cliente, that.cliente);
+	}
+
+	public int hashCode() {
+		return java.util.Objects.hash(super.hashCode(), fecha_apertura, fecha_cierre, clasificacion, cliente);
+	}
+
+	@java.lang.Override
+	public java.lang.String toString() {
+		return "Cuenta_Fintech{" +
+				"fecha_apertura=" + fecha_apertura +
 				", fecha_cierre=" + fecha_cierre +
 				", clasificacion='" + clasificacion + '\'' +
 				", cliente=" + cliente +
