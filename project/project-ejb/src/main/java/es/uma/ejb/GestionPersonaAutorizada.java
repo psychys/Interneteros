2ee9e9fbc82bc58814a7c87ac75f1b2ce288c69d
xVenom <lastareasdeilias@gmail.com>
2022-05-01 04:27:01 +0200

    añadidas anotaciones de requisitos

--- project/project-ejb/src/main/java/es/uma/ejb/GestionPersonaAutorizada.java
+++ project/project-ejb/src/main/java/es/uma/ejb/GestionPersonaAutorizada.java
@@ -9,12 +9,15 @@ public interface GestionPersonaAutorizada {
 
     public void CrearPersonaAutorizada(Persona_autorizada c, Usuario admin) throws PersonaAutorizadaException;
 
+    //@Requisitos({"RF6"})
     public void AniadirPersonaAutorizada(Persona_autorizada pa, Cuenta_Fintech c_fin, Usuario admin) throws PersonaAutorizadaException;
 
+    //@Requisitos({"RF7"})
     public void ActualizarPersonaAutorizada(Persona_autorizada c, Usuario admin) throws PersonaAutorizadaException;
 
     public Persona_autorizada BuscarPersonaAutorizada(int id, Usuario admin) throws PersonaAutorizadaException;
 
+    //@Requisitos({"RF8"})
     public void MarcarPersonaAutorizada(Persona_autorizada c, String s, Usuario admin) throws PersonaAutorizadaException;
 
 }
