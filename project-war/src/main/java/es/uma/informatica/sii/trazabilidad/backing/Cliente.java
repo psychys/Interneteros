1c65ac1812ba0049823bc3a1175ad4a6db4be6b4
carlosqc117 <quesadacarlos117@gmail.com>
2022-05-24 12:22:49 +0200

    Consultar datos del cliente desde vista cliente, puesto bonito, falta saber extraer los datos

--- project-war/src/main/java/es/uma/informatica/sii/trazabilidad/backing/Cliente.java
+++ project-war/src/main/java/es/uma/informatica/sii/trazabilidad/backing/Cliente.java
@@ -6,6 +6,7 @@ import es.uma.interneteros.ejb.GestionCliente;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.Date;
 
 @Named(value= "Cliente")
 @RequestScoped
@@ -17,13 +18,38 @@ public class Cliente {
     @Inject
     private InfoSesion sesion;
 
-
-
     private Cliente cliente;
 
+    public String getNombre(){
+        return cliente.getNombre();
+    }
 
+    public String getApellido(){
+        return cliente.getApellido();
+    }
 
+    public String getIdentificacion(){
+        return cliente.getIdentificacion();
+    }
 
+    public Date getFecha(){
+        return cliente.getFecha();
+    }
 
+    public String getDireccion(){
+        return cliente.getDireccion();
+    }
+
+    public String getCiudad(){
+        return cliente.getCiudad();
+    }
+
+    public int getCP(){
+        return cliente.getCP();
+    }
+
+    public String getPais(){
+        return cliente.getPais();
+    }
 
 }
